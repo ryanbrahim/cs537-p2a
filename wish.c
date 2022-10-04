8a1d491042ffd95946c20d91837dbaa90f71a8ab
almizyed <almizyed@wisc.edu>
2022-10-04 17:15:07 -0500

    Initial project

---
+++ wish.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <wish.h>
+
+int main()
+{
+	test_func();
+	return 0;
+}
+
---
+++ wish.h
@@ -0,0 +1,14 @@
+
+#ifndef WISH_H
+#define WISH_H
+
+void test_func()
+{
+    printf("Hello World in the h!\n");
+    
+}
+
+
+
+#endif
+
